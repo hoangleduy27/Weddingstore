dfde85c1084308f3c8c458b3fccccf805bc83828
hoangleduy27 <70272512+hoangleduy27@users.noreply.github.com>
2022-05-31 01:49:09 +0700

    Delete QLadmin directory

--- QLadmin/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-$db = mysqli_connect('localhost', 'root', '', 'demo_db') or die('Không thể kết nói với database');
-unset($_SESSION["quyenhan"]);
-header("Location:signIn.php");
-?>
